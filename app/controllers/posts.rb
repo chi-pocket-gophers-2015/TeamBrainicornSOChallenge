c793e09448cd810fc9afb1fa187122af8c9ebb17
George Lee & petermanhart & Nathan Werbeckes <pair+ghrlee.petermanhart.werbeckes@devbootcamp.com>
2015-05-06 15:55:34 -0500

    add new question form

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -9,15 +9,33 @@ get '/index' do
   erb :index
 end
 
+get '/questions/new' do
+  puts "current user: #{current_user.username}"
+  erb :"posts/new"
+end
+
+post '/questions' do
+  puts params.inspect
+  puts "user id: #{session[:user_id]}"
+  puts "current user: #{current_user}"
+  @question = current_user.questions.create(params)
+
+  # @question = Question.new
+  redirect '/index'
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @answers = @question.answers
   erb :"posts/question"
 end
 
+post '/questions/:id/answers' do
+  redirect '/questions/:id'
+end
+
+
 
 # this goes with the new-comment ajax request in application.js
 # get '/comments/new' do
-
-
 # end
