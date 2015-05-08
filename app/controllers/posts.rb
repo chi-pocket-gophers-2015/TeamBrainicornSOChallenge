bb96f29ce6be119902e3cca8d4f39c7cea6692a2
Nathan Werbeckes <werbeckes@gmail.com>
2015-05-07 20:26:00 -0500

    Sorting complete for questions on index

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -3,7 +3,9 @@ get '/' do
 end
 
 get '/index' do
-  @questions = Question.all
+  sort_pref = get_sort_method
+  @questions = Question.get_all_sorted(sort_pref)
+
   @answer_counts = @questions.map{ |question| question.answers.count }
   @scores = @questions.map{ |question| question.score }
   erb :index
@@ -17,6 +19,38 @@ get '/questions/new' do
   puts "current user: #{current_user.username}"
   erb :"posts/new"
 end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  @answers = @question.answers
+  @best_answer = @question.best_answer
+  erb :"posts/question"
+end
+
+get '/comments/new' do
+  if request.xhr?
+    erb :'partials/_new_comment_form', layout: false
+  else
+    erb :'partials/_new_comment_form'
+  end
+end
+
+
+get '/sort_by/votes' do
+  session[:sorting] = "votes"
+  redirect '/'
+end
+
+get '/sort_by/recent' do
+  session[:sorting] = "recent"
+  redirect '/'
+end
+
+get '/sort_by/trending' do
+  session[:sorting] = "trending"
+  redirect '/'
+end
+
 
 post '/questions' do
   puts params.inspect
@@ -37,30 +71,6 @@ post '/questions/:id/answers' do
   end
 end
 
-get '/questions/:id' do
-  @question = Question.find(params[:id])
-  @answers = @question.answers
-  @best_answer = @question.best_answer
-  erb :"posts/question"
-end
-
-post '/questions/:question_id/answers/:answer_id/best' do
-  question = Question.find(params[:question_id].to_i)
-  question.update_attributes(best_answer_id: params[:answer_id].to_i)
-  redirect "/questions/#{params[:question_id]}"
-end
-
-
-
-# this goes with the new-comment ajax request in application.js
-get '/comments/new' do
-  if request.xhr?
-    erb :'partials/_new_comment_form', layout: false
-  else
-    erb :'partials/_new_comment_form'
-  end
-end
-
 post '/comments' do
   params[:commentable_type].capitalize!
   puts "params: #{params}"
@@ -72,4 +82,9 @@ post '/comments' do
   end
 end
 
+post '/questions/:question_id/answers/:answer_id/best' do
+  question = Question.find(params[:question_id].to_i)
+  question.update_attributes(best_answer_id: params[:answer_id].to_i)
+  redirect "/questions/#{params[:question_id]}"
+end
 
